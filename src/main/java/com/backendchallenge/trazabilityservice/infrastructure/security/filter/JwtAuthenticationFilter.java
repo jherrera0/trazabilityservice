e0cfd0caa98918d65b964962e86658540bb98faa
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 17:14:28 -0500

    implement all the logic of authentication

---
+++ src/main/java/com/backendchallenge/trazabilityservice/infrastructure/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.backendchallenge.trazabilityservice.infrastructure.security.filter;
+
+import com.backendchallenge.trazabilityservice.domain.until.ConstJwt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserDetailsService myUserDetailsService;
+    @Override
+    public void doFilterInternal(HttpServletRequest request,
+                                 @NonNull HttpServletResponse response,
+                                 @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader(ConstJwt.HEADER_STRING);
+
+        if (authHeader == null || !authHeader.startsWith(ConstJwt.BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        try {
+            String jwt = authHeader.substring(ConstJwt.LINESTRING_INDEX);
+            UserDetails user = myUserDetailsService.loadUserByUsername(jwt);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, jwt,
+                    user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write(e.getMessage());
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
