05f30ae1a9a2097d76a61600f368e5870923a3fa
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 17:05:10 -0500

    Initial commit

---
+++ src/test/java/com/backendchallenge/trazabilityservice/TrazabilityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendchallenge.trazabilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrazabilityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
