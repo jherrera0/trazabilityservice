ac3ad7a6fce920883f9c96c3103485f83900c35f
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-05 01:01:28 -0500

    implement unit test on the application layer

--- src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstTest.java
+++ src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstTest.java
@@ -1,9 +1,11 @@
 package com.backendchallenge.traceabilityservice.domain.until;
 
+
 public class ConstTest {
     public static final String VALID_TOKEN = "valid.token.here";
     public static final String INVALID_TOKEN = "invalid.token.here";
     public static final Long ID_TEST = 1L;
+    public static final  String STATUS_TEST = "status";
 
     private ConstTest() {
     }
--- src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstValidation.java
+++ src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstValidation.java
@@ -2,6 +2,8 @@ package com.backendchallenge.traceabilityservice.domain.until;
 
 public class ConstValidation {
     public static final Integer ZERO = 0;
+    public static final int ONE = 1;
+
     private ConstValidation() {
     }
 }
