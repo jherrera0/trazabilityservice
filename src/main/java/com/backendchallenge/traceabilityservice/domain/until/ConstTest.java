858c29c2dd0287ac2951b06e7d753c02e95b0b0b
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-05 00:49:29 -0500

    implement unit test on the infrastructure

--- src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstTest.java
+++ src/main/java/com/backendchallenge/traceabilityservice/domain/until/ConstTest.java
@@ -3,6 +3,7 @@ package com.backendchallenge.traceabilityservice.domain.until;
 public class ConstTest {
     public static final String VALID_TOKEN = "valid.token.here";
     public static final String INVALID_TOKEN = "invalid.token.here";
+    public static final Long ID_TEST = 1L;
 
     private ConstTest() {
     }
